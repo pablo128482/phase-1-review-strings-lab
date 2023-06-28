05aefa48a701fe125a124c3617688c4c14a3586a
pablo128482 <ashley.c.clay@gmail.com>
2023-06-28 12:43:42 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+const welcomeMessage = 'Welcome to Flatbook, currentUser, Grace Hopper!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser, GRACE HOPPER!';
+const shortGreeting = 'Welcome, G!';
